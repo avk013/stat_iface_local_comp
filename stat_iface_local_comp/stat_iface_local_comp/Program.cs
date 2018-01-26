264536c53e8ac64a32a91ca6c985185996ab208a
Kovalenko Alexander <avk013@live.ru>
2018-01-26 05:07:08 +0200

    1

---
+++ stat_iface_local_comp/stat_iface_local_comp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stat_iface_local_comp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //123
+        }
+    }
+}
