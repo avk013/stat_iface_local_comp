af261ae253bab02ac65ff810f4688c1ec9b3c06b
Kovalenko Alexander <avk013@live.ru>
2018-02-03 10:10:23 +0200

    1

--- stat_iface_local_comp/stat_iface_local_comp/Program.cs
+++ stat_iface_local_comp/stat_iface_local_comp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,21 @@ namespace stat_iface_local_comp
 {
     class Program
     {
-        static void Main(string[] args)
+        static string path_conf_file = @"c:\stat_iface_local_comp\!!!out\hosts.conf";
+        static string path_conf = @"c:\stat_iface_local_comp\!!!out";
+       // static string path = @"c:\stat_iface_local_comp";
+        static void init_config()
         {
+            //if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+            if (!Directory.Exists(path_conf)) Directory.CreateDirectory(path_conf);
+            if (!File.Exists(path_conf_file)) File.Create(path_conf_file);
+            FileInfo f = new FileInfo(path_conf_file);
+        }
+        public static void Main(string[] args)
+        {
+           init_config();
             //123
         }
-    }
+         }
+    
 }
